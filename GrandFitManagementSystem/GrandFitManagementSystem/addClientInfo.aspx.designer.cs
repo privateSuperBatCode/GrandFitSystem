0799f0fddc1c2a80e54c52abdeb08e73eeff8bba
SuperBatCode <contact@superbatcode.com>
2014-06-09 15:29:27 +0530

    don't bother

--- GrandFitManagementSystem/GrandFitManagementSystem/addClientInfo.aspx.designer.cs
+++ GrandFitManagementSystem/GrandFitManagementSystem/addClientInfo.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace GrandFitManagementSystem {
     public partial class addClientInfo {
         
         /// <summary>
+        /// sample_1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm sample_1;
+        
+        /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
